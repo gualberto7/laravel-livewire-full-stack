6bee5b938f80c6ce5bc8c72487f244220090e1f8
gcuiza <gualbertocuizav@gmail.com>
2025-04-28 19:26:40 -0400

    checkins table

--- resources/views/livewire/client/check-ins.blade.php
+++ resources/views/livewire/client/check-ins.blade.php
@@ -5,10 +5,18 @@ use Livewire\Volt\Component;
 
 new class extends Component {
     public Client $client;
+    public $currentGym;
+    public $checkIns;
 
     public function mount(Client $client)
     {
         $this->client = $client;
+        $this->currentGym = auth()->user()->getCurrentGym();
+        $this->checkIns = $client->checkIns()
+            ->where('gym_id', $this->currentGym->id)
+            ->latest()
+            ->take(15)
+            ->get();
     }
 }
 
@@ -20,13 +28,43 @@ new class extends Component {
     <x-clients.layout :client="$client">
         <x-slot:heading>
             Check-ins
-    </x-slot>
-    
-    <x-slot:subheading>
-        Check-ins
-    </x-slot>
+        </x-slot>
 
-    <div>
-    </div>
+        <div>
+            <x-gc.table>
+                <x-slot:header>
+                    <tr>
+                        <x-gc.th>
+                            Fecha
+                        </x-gc.th>
+                        <x-gc.th>
+                            Hora
+                        </x-gc.th>
+                        <x-gc.th>
+                            Registrado por
+                        </x-gc.th>
+                    </tr>
+                </x-slot:header>
+                @forelse ($checkIns as $checkIn)
+                    <tr>
+                        <x-gc.td>
+                            {{ $checkIn->created_at->format('d/m/Y') }}
+                        </x-gc.td>
+                        <x-gc.td>
+                            {{ $checkIn->created_at->format('H:i') }}
+                        </x-gc.td>
+                        <x-gc.td>
+                            {{ $checkIn->created_by }}
+                        </x-gc.td>
+                    </tr>
+                @empty
+                    <tr>
+                        <x-gc.td colspan="4" class="text-center">
+                            No hay check-ins.
+                        </x-gc.td>
+                    </tr>
+                @endforelse
+            </x-gc.table>
+        </div>
     </x-clients.layout>
 </div>
