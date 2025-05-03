91aaec124343f6df3f99ee0b9e2790aa6085fc81
gcuiza <gualbertocuizav@gmail.com>
2025-05-03 10:36:03 -0400

    subscription model test

---
+++ tests/Unit/Models/SubscriptionModelTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\Gym;
+use App\Models\Client;
+use App\Models\Membership;
+use App\Models\Subscription;
+
+test('subscription belongs to a client', function () {
+    $subscription = Subscription::factory()->create();
+
+    expect($subscription->client)->toBeInstanceOf(Client::class);
+});
+
+test('subscription belongs to a membership', function () {
+    $subscription = Subscription::factory()->create();
+
+    expect($subscription->membership)->toBeInstanceOf(Membership::class);
+});
+
+test('subscription belongs to a gym', function () {
+    $subscription = Subscription::factory()->create();
+
+    expect($subscription->gym)->toBeInstanceOf(Gym::class);
+});
+
+test('subscription return active status', function () {
+    $subscription = Subscription::factory()->create([
+        'start_date' => now()->subDay(),
+        'end_date' => now()->addDay(),
+    ]);
+
+    expect($subscription->getStatus())->toBe('activa');
+});
+
+test('subscription return pending status', function () {
+    $subscription = Subscription::factory()->create([
+        'start_date' => now()->addDay(),
+        'end_date' => now()->addDays(2),
+    ]);
+
+    expect($subscription->getStatus())->toBe('pendiente');
+});
+
+test('subscription return expired status', function () {
+    $subscription = Subscription::factory()->create([
+        'start_date' => now()->subDays(2),
+        'end_date' => now()->subDay(),
+    ]);
+
+    expect($subscription->getStatus())->toBe('vencida');
+});
+    
